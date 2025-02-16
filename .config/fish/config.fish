a8459917c7f3f2d3a8ec5190a7ff6b50807e5c2a
Clayton Ramstedt <clayton.ramstedt@gmail.com>
2025-02-16 10:26:34 -0700

    added aliases to config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,3 +5,15 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 #function fish_greeting
 #    # smth smth
 #end
+
+# tmux
+alias tma="tmux a"
+alias tmn="tmux new"
+
+# git
+alias gst="git status"
+alias gcm="git commit -m "
+alias ga.="git add ."
+
+# ls
+alias ll="ls -la"
